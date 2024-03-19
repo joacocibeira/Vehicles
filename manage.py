59fdd8fc0b3085827bd3efc91e00dbfcd1a6f338
joacocibeira <Cibeira.joaquin@gmail.com>
2024-03-19 20:27:28 +0100

    basic project structure

--- manage.py
+++ manage.py
@@ -2,11 +2,15 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from Vehicles.settings import base
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vehicles.settings')
+    if base.DEBUG == True:
+            os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vehicles.settings.local')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vehicles.settings.production')
+        
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
